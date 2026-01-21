429da1ccd1f4b11c4e1114ce5156cf8946191e9c
seunglee10 <ryoograce25@gmail.com>
2026-01-22 00:40:16 +0900

    수정하기카드 크기조정

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 from bson import ObjectId
 
 app = Flask(__name__)
+
 client = MongoClient("mongodb://localhost:27017")
 db = client.dbjungle
 memos = db.memos
 
-# 메모 조회
+
+# 메인 페이지 (좋아요 내림차순)
 @app.route("/")
 def home():
     memo_list = list(memos.find().sort("likes", -1))
@@ -19,67 +21,43 @@ def home():
 # 메모 저장
 @app.route("/memo", methods=["POST"])
 def save_memo():
-    title = request.form.get("title")
-    content = request.form.get("content")
-
-    memo = {
-        "title": title,
-        "content": content,
+    memos.insert_one({
+        "title": request.form["title"],
+        "content": request.form["content"],
         "likes": 0
-    }
-    memos.insert_one(memo)
-    return redirect(url_for("home"))
+    })
+    return jsonify({"result": "success"})
 
 
 # 메모 수정
 @app.route("/update/<memo_id>", methods=["PATCH"])
 def update_memo(memo_id):
-    title = request.form.get("title")
-    content = request.form.get("content")
-
     memos.update_one(
         {"_id": ObjectId(memo_id)},
         {"$set": {
-            "title": title,
-            "content": content
+            "title": request.form["title"],
+            "content": request.form["content"]
         }}
     )
-    return redirect(url_for("home"))
+    return jsonify({"result": "success"})
 
 
 # 메모 삭제
-@app.route("/delete/<memo_id>")
+@app.route("/delete/<memo_id>", methods=["DELETE"])
 def delete_memo(memo_id):
-    memos.delete_one(
-        {"_id": ObjectId(memo_id)}
-    )
-    return redirect(url_for("home"))
+    memos.delete_one({"_id": ObjectId(memo_id)})
+    return jsonify({"result": "success"})
 
 
-# 좋아요 수 증가
-@app.route("/like/<memo_id>")
+# 좋아요 증가
+@app.route("/like/<memo_id>", methods=["POST"])
 def like_memo(memo_id):
     memos.update_one(
         {"_id": ObjectId(memo_id)},
         {"$inc": {"likes": 1}}
     )
-    return redirect(url_for("home")) 
-
-
-# 좋아요 순으로 조회
-@app.route("/edit/<memo_id>")
-def edit_memo(memo_id):
-    memo_list = list(memos.find().sort("likes", -1))
-    for memo in memo_list:
-        memo["_id"] = str(memo["_id"])
-        
-    return render_template(
-        "index.html",
-        memos=memo_list,
-        edit_id=memo_id
-    )
+    return jsonify({"result": "success"})
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+    app.run(debug=True)
